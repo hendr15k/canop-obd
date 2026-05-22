5326c2908acb1b4a34e701b207435620f79f69b1
hendrik schmitz <hendrik.schmitz03.sh@gmail.com>
2026-05-22 12:38:07 +0000

    ci: add detekt and ktlint gradle plugins
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
-// Top-level build file
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.4" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
 }
